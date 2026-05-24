c274e05baa24e6fc721659bd7cd17a4bec61bf3b
Thies Ohme <contact@mcds.moe>
2026-05-25 01:56:49 +0200

    Added Murder Drones + Github CICD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import lucode from "lucode-starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  site: "https://arg.mcds.moe",
+  integrations: [
+    starlight({
+      title: "Minecodes' ARG Wiki",
+      plugins: [
+        lucode({
+          navLinks: [
+            {
+              label: "Murder Drones",
+              link: "/glitch/murder-drones/disclaimer",
+            },
+          ],
+        }),
+      ],
+      social: [
+        {
+          icon: "codeberg",
+          label: "Codeberg",
+          href: "https://codeberg.org/Minecodes/arg.mcds.moe",
+        },
+      ],
+      /**sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          items: [{ autogenerate: { directory: "reference" } }],
+        },
+      ],**/
+    }),
+  ],
 });
